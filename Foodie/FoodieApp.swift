2c91e557940b8260a483071ffcaace9418578b60
Oleksandra Pinchuk <ppinchukk.25@gmail.com>
2022-10-16 19:04:29 +0300

    Initial Commit

---
+++ Foodie/FoodieApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodieApp.swift
+//  Foodie
+//
+//  Created by Oleksandra Pinchuk on 16.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct FoodieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
